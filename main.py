eee3a98cb93b9e014b3e81f48448f765e91637d0
Anas Ahmad Siddiqui <anassiddiqui768@gmail.com>
2023-04-04 02:09:25 +0530

    edited main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-CORS(app, origins='*')
+# CORS(app, origins='*')
 
-@app.after_request
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    return response
+# @app.after_request
+# def add_cors_headers(response):
+#     response.headers['Access-Control-Allow-Origin'] = '*'
+#     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+#     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+#     return response
 
 @app.route('/urls', methods=['POST'])
 def urls():
@@ -23,7 +23,7 @@ def urls():
         return jsonify({'error': 'Invalid URL'})
     else:
         print(score)
-        data = {'Obscenity Confidence': score}
+        data = {'obscenity_score': str(score)}
         response = jsonify(data)
         response.status_code = 200
         return response
